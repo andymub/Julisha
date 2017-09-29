a5fc23ca95bffd26827ed384555c3e7a06826cb2
Andymub <andymub1@gmail.com>
2017-09-29 16:08:08 +0100

    10- entreprise activity

---
+++ app/src/main/java/com/julisha/afrisuser/julisha/EntreppriseListeActivity.java
@@ -0,0 +1,55 @@
+package com.julisha.afrisuser.julisha;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class EntreppriseListeActivity extends AppCompatActivity {
+
+    ListView list;
+    String[] itemname ={
+            "Bralima",
+            "Orange",
+            "Africell",
+            "Equity",
+            "Airtel",
+            "Vodacom",
+            "SCPT",
+            "SNEL"
+    };
+
+
+    Integer[] imgid={
+            R.drawable.pic_bralima,
+            R.drawable.pic_orange,
+            R.drawable.pic_africel,
+            R.drawable.pic_equity,
+            R.drawable.pic_airtel,
+            R.drawable.pic_vodacom,
+            R.drawable.pic_scpt,
+            R.drawable.pic_snel   };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_entrepprise_liste);
+        CustomListAdapter adapter=new CustomListAdapter(this, itemname, imgid);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // TODO Auto-generated method stub
+                String Slecteditem= itemname[+position];
+                Toast.makeText(getApplicationContext(), Slecteditem, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/julisha/afrisuser/julisha/PrincipalActivity.java
+++ app/src/main/java/com/julisha/afrisuser/julisha/PrincipalActivity.java
@@ -3,6 +3,7 @@ package com.julisha.afrisuser.julisha;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.ImageButton;
 
@@ -22,6 +23,19 @@ public class PrincipalActivity extends AppCompatActivity implements View.OnClick
 
         //set onclick btn
         btnProject.setOnClickListener(this);
+        btnEvents.setOnClickListener(this);
+        btnEntreprise.setOnClickListener(this);
+        btnProfil.setOnClickListener(this);
+
+        // Set the third image button background transparent
+        // This method allow us to show click effect
+        TypedValue outValue = new TypedValue();
+        getApplicationContext().getTheme().resolveAttribute(
+                android.R.attr.selectableItemBackground, outValue, true);
+        btnProfil.setBackgroundResource(outValue.resourceId);
+        btnEntreprise.setBackgroundResource(outValue.resourceId);
+        btnEvents.setBackgroundResource(outValue.resourceId);
+        btnProject.setBackgroundResource(outValue.resourceId);
     }
 
 
@@ -32,10 +46,12 @@ public class PrincipalActivity extends AppCompatActivity implements View.OnClick
         {
             case R.id .imageButtonEntreprise :
                 //todo go to entreprise activity
+                Intent IntentEntreprise = new Intent(PrincipalActivity.this, EntreppriseListeActivity.class);
+                startActivity(IntentEntreprise);
                 break;
             case R.id .imageButtonprofil :
                 //todo go to Profil activity
-                principalIntent= new Intent(PrincipalActivity.this, CreateProfil1Activity.class);
+                principalIntent= new Intent(PrincipalActivity.this, UserProfilActivity.class);
                 startActivity(principalIntent);
                 break;
             case R.id .imageButtonproject :
